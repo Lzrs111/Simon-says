fda96dfbfd54300a556bc75bc77ff3ba3f96d4d5
Mario <mario.dragovic@gmail.com>
2017-08-10 16:08:56 +0200

    Added sounds to animation

--- src/animationFunction.js
+++ src/animationFunction.js
@@ -1,4 +1,8 @@
 import rgbToHex from "./rgbtohex.js"
+import SimonSound1 from "./simonSound1.mp3"
+import SimonSound2 from "./simonSound2.mp3"
+import SimonSound3 from "./simonSound3.mp3"
+import SimonSound4 from "./simonSound4.mp3"
 
 var rgbHex = rgbToHex //calling the function directly raises an error
 var colors = {
@@ -8,11 +12,11 @@ var colors = {
         "1e3c72":  "#2a52CC" 
     }
 var sounds = {
-        "999900": "./SimonSound1.mp3",
-        "004600": "./SimonSound2.mp3",
-        "8b0000": "./SimonSound3.mp3",
-        "1e3c72": "./SimonSound4.mp3"
-    }
+        "999900": SimonSound1, 
+        "004600": SimonSound2,
+        "8b0000": SimonSound3, 
+        "1e3c72": SimonSound4
+        }
 
 export default function animationFunction(element) {
     var color = window.getComputedStyle(element, null).getPropertyValue("background-color").match(/\d{1,3}/g)
